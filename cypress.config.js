3cae4bc6d41955882bba23a55e8f5cd0ab99f09f
AzimzadaMurad <azimzadamurad@gmail.com>
2023-06-25 16:48:31 +0400

    update config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  experimentalEventsTap: true,
+  watchForFileChanges: false,
+  numTestsKeptInMemory: 0,
+  env: {
+    NODE_OPTIONS: "--max-http-header-size=16384",
+  },
+  nodeOptions: "--max-old-space-size=4096",
+  emitter: {
+    setMaxListeners: 50,
+  },
   e2e: {
     defaultCommandTimeout: 10000,
     baseUrl: "https://www.saucedemo.com",
